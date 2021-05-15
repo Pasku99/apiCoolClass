5f9340819fb2f26cc6c2826d5995bbedccca4952
Pascual <pasku99albatera@gmail.com>
2021-05-15 10:36:56 +0200

    nuevos cambios

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use('/api/examenes', require('./routes/examen'));
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, process.env.HOST || '0.0.0.0', () => {
-    console.log('Servidor corriendo en el puerto ' + process.env.PORT || 3000);
+app.listen(port, host, () => {
+    console.log('Servidor corriendo en el puerto ' + port);
 });
